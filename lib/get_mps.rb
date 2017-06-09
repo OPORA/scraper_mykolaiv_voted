83f491f801efdd9dab569d146f70d2b84e07dca4
Beastie PC <gskondratenko@opora.org.ua>
2017-06-10 02:49:33 +0300

    Change scraper for lutsk #1

--- lib/get_mps.rb
+++ lib/get_mps.rb
@@ -2,12 +2,20 @@ require 'open-uri'
 require 'json'
 class GetMp
   def initialize
-    @data_hash = JSON.load(open('http://lvivmp.oporaua.org/'))
+    @data_hash = JSON.load(open('https://scraperlutskdeputy.herokuapp.com/'))
   end
   def serch_mp(full_name)
-    p full_name
-   name =full_name.gsub(/'/,'’').gsub(/ Депутатські повноваження складено/,'')
-   data = @data_hash.find {|k| k["full_name"] == name  }
-   return data["deputy_id"]
+     p full_name
+   if full_name == "Рабан МикитаТарасович"
+     name = "Рабан Микита Тарасович"
+   else
+     name =full_name.gsub(/'/,'’').gsub(/ Депутатські повноваження складено/,'')
+   end
+   if full_name == "Романюк Микола Ярославович"
+     return 1112
+   else
+     data = @data_hash.find {|k| k["full_name"] == name  }
+     return data["deputy_id"]
+   end
   end
 end
--- lib/read_file.rb
+++ lib/read_file.rb
@@ -1,23 +1,21 @@
 require 'yomu'
 class ReadFile
   def rtf(file_name)
+    p file_name
     votes = []
     yomu = Yomu.new file_name
-     if yomu.text[/\nУКРАЇНА\n\sЛЬВІВСЬКА МІСЬКА РАДА/]
-       text_pages = yomu.text.split(/\nУКРАЇНА\n\sЛЬВІВСЬКА МІСЬКА РАДА/)
-     elsif yomu.text[/ЛЬВІВСЬКА МІСЬКА РАДА\n\n ПОІМЕННЕ ГОЛОСУВАННЯ/]
-       text_pages = yomu.text.split(/ЛЬВІВСЬКА МІСЬКА РАДА\n\n ПОІМЕННЕ ГОЛОСУВАННЯ/)
-     elsif yomu.text[/ЛЬВІВСЬКА МІСЬКА РАДА\n\n ВІДКРИТЕ ГОЛОСУВАННЯ/]
-       text_pages = yomu.text.split(/ЛЬВІВСЬКА МІСЬКА РАДА\n\n ВІДКРИТЕ ГОЛОСУВАННЯ/)
-     else
-       raise yomu.text
-     end
-    text_pages.each do |page|
-     next if page == ""
-     next if page[/^\n\s$/]
+    page = yomu.text
+
      vote = {}
      vote[:datetime] = page[/^від.+/].gsub(/від/,'').strip
-     # vote[:name] = yomu.text.split(/\n/).find{|str| str.strip[/^\d+\.\s/]}.strip.gsub(/^\d+\.\s/,'').strip
+     unless page[/ГОЛОСУВАННЯ НЕ ПРОВОДИЛОСЬ/]
+       vote[:res] = page[/РІШЕННЯ.+/].strip
+     else
+       vote[:res] = "ГОЛОСУВАННЯ НЕ ПРОВОДИЛОСЬ"
+     end
+
+     vote[:name] = page.split(/\n/).find{|str| str.strip[/^\d+\.\s/]}.strip.gsub(/^\d+\.\s/,'').strip
+     # vote[:number] = page.split(/\n/).find{|str| str.strip[/^\d+\.\s/]}.strip[/^\d+/]
      vote[:voteds] = []
      paragraf =  page.gsub(/\n/, '\n')
      paragraf[/Вибір.+(УСЬОГО:|ВСЬОГО:)/].gsub(/\s{2,}/, ' ').gsub(/(Вибір|УСЬОГО:|ВСЬОГО:)/,'').split(/\\n/).each do |v|
@@ -30,7 +28,6 @@ class ReadFile
        end
      end
     votes << vote
-    end
     return votes
   end
 end
